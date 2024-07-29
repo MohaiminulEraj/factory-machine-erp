3f78101da38d26b1a21e3ab611fbe366884035bf
MohaiminulEraj <eraj471@gmail.com>
2024-07-29 23:27:12 +0600

    auth module added

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from 'src/config/jwt.config';
+import { User } from '../users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from './service/jwt.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync(jwtConfig),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService, JwtStrategy, ConfigService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,150 +1,177 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
-import { LoginDto, RegistrationDto } from '../dto/auth.dto';
-import { User } from 'src/modules/users/entities/user.entity';
-import { JwtService } from './jwt.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
+import { LoginDto, RegistrationDto, UpdatePasswordDto } from '../dto/auth.dto'
+import { User } from 'src/modules/users/entities/user.entity'
+import { JwtService } from './jwt.service'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 import * as bcrypt from 'bcryptjs'
 import * as crypto from 'crypto'
-import { LoggedInUser } from 'src/modules/users/data/logged-in-user.type';
+import { LoggedInUser } from 'src/modules/users/data/logged-in-user.type'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-    @Inject(JwtService)
-    private readonly jwtService: JwtService,
-  ) {}
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        @Inject(JwtService)
+        private readonly jwtService: JwtService
+    ) {}
 
-  async login(loginDto: LoginDto) {
-    const user: User = await this.getAUser({
-      employeeId: loginDto.employeeId
-    })
+    async login(loginDto: LoginDto) {
+        const user: User = await this.getAUser({
+            employeeId: loginDto.employeeId
+        })
 
-    if (!user) {
-        throw new HttpException(
-            'Invalid User credentials',
-            HttpStatus.BAD_REQUEST
-        )
-    }
-    try {
-        const isPasswordValid: boolean = this.jwtService.isPasswordValid(
-            loginDto.password,
-            user.password
-        )
-        if (!isPasswordValid) {
+        if (!user) {
             throw new HttpException(
                 'Invalid User credentials',
                 HttpStatus.BAD_REQUEST
             )
         }
-    } catch (error) {
-        throw new HttpException(
-            'Invalid User credentials',
-            HttpStatus.BAD_REQUEST
-        )
-    }
-    user.lastLogin = Date.now()
-    await user.save()
-    
-    return await this.unifiedAuthResponse(user)
-  }
-
-  async registration(registrationDto: RegistrationDto) {
-    if (registrationDto.password != registrationDto.confirmPassword) {
-        throw new HttpException(
-            'Password Mismatched',
-            HttpStatus.BAD_REQUEST
-        )
-    }
-    const previousData = await this.userRepository.findOne({
-        where: {
-            employeeId: registrationDto.employeeId
+        try {
+            const isPasswordValid: boolean = this.jwtService.isPasswordValid(
+                loginDto.password,
+                user.password
+            )
+            if (!isPasswordValid) {
+                throw new HttpException(
+                    'Invalid User credentials',
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+        } catch (error) {
+            throw new HttpException(
+                'Invalid User credentials',
+                HttpStatus.BAD_REQUEST
+            )
         }
-    })
+        user.lastLogin = Date.now()
+        await user.save()
 
-    if (previousData) {
-        throw new HttpException('Employee Id already exists', HttpStatus.CONFLICT)
+        return await this.unifiedAuthResponse(user)
     }
 
-    try {
-        let userToRegister: User
+    async registration(registrationDto: RegistrationDto) {
+        if (registrationDto.password != registrationDto.confirmPassword) {
+            throw new HttpException(
+                'Password Mismatched',
+                HttpStatus.BAD_REQUEST
+            )
+        }
+        const previousData = await this.userRepository.findOne({
+            where: {
+                employeeId: registrationDto.employeeId
+            }
+        })
 
-        const salt = await bcrypt.genSaltSync(10)
-        const hashedPassword = await bcrypt.hash(
-            registrationDto.password,
-            salt
-        )
+        if (previousData) {
+            throw new HttpException(
+                'Employee Id already exists',
+                HttpStatus.CONFLICT
+            )
+        }
 
-        userToRegister = new User()
-        userToRegister.name = registrationDto.name
-        userToRegister.employeeId = registrationDto.employeeId
-        userToRegister.password = hashedPassword
+        try {
+            let userToRegister: User
 
-        const registeredUser =
-            await this.userRepository.save(userToRegister)
+            const salt = await bcrypt.genSaltSync(10)
+            const hashedPassword = await bcrypt.hash(
+                registrationDto.password,
+                salt
+            )
 
-        return await this.unifiedAuthResponse(registeredUser)
-    } catch (error) {
-        throw new HttpException(
-            'An error occurred while registering a user',
-            HttpStatus.BAD_REQUEST
-        )
+            userToRegister = new User()
+            userToRegister.name = registrationDto.name
+            userToRegister.employeeId = registrationDto.employeeId
+            userToRegister.password = hashedPassword
+
+            const registeredUser =
+                await this.userRepository.save(userToRegister)
+
+            return await this.unifiedAuthResponse(registeredUser)
+        } catch (error) {
+            throw new HttpException(
+                'An error occurred while registering a user',
+                HttpStatus.BAD_REQUEST
+            )
+        }
     }
-}
 
+    findAll() {
+        return `This action returns all auth`
+    }
 
+    findOne(id: number) {
+        return `This action returns a #${id} auth`
+    }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  /**
+    /**
      * GET A USER
      */
-  async getAUser(condition: any) {
-    return await this.userRepository.findOne({
-        select: {
-            id: true,
-            uuid: true,
-            password: true,
-            name: true,
-            lastLogin: true
-        },
-        where: condition
-    })
-}
-
-/**
- * UNIFIED RESPONSE FOR AUTH
- */
-async unifiedAuthResponse(user: User | any) {
-    const token: string = this.jwtService.generateToken(user)
-    return {
-        id: user.id,
-        uuid: user.uuid,
-        employeeId: user.employeeId,
-        name: user.name,
-        token: token,
+    async getAUser(condition: any) {
+        return await this.userRepository.findOne({
+            select: {
+                id: true,
+                password: true,
+                name: true,
+                lastLogin: true
+            },
+            where: condition
+        })
     }
-}
 
-public async refreshToken(loggedInUser: LoggedInUser) {
-  const user = await this.unifiedAuthResponse(
-      await this.getAUser({ id: loggedInUser.id })
-  )
-  return user
-}
-  // update(id: number, updateAuthDto: UpdateAuthDto) {
-  //   return `This action updates a #${id} auth`;
-  // }
+    /**
+     * UNIFIED RESPONSE FOR AUTH
+     */
+    async unifiedAuthResponse(user: User | any) {
+        const token: string = this.jwtService.generateToken(user)
+        return {
+            id: user.id,
+            employeeId: user.employeeId,
+            name: user.name,
+            token: token
+        }
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+    async updateUserPassword(
+        userId: number,
+        updateUserPasswordDto: UpdatePasswordDto
+    ) {
+        const user = await this.userRepository.findOne({
+            select: ['id', 'password'],
+            where: {
+                id: userId
+            }
+        })
+        if (!user) {
+            throw new HttpException('Invalid user id', HttpStatus.BAD_REQUEST)
+        }
+
+        const isPasswordValid = this.jwtService.isPasswordValid(
+            updateUserPasswordDto.currentPassword,
+            user.password
+        )
+        if (!isPasswordValid) {
+            throw new HttpException(
+                'Current password is incorrect',
+                HttpStatus.BAD_REQUEST
+            )
+        }
+
+        user.password = this.jwtService.encodePassword(
+            updateUserPasswordDto.newPassword
+        )
+        return await this.userRepository.save(user)
+    }
+
+    public async refreshToken(loggedInUser: LoggedInUser) {
+        const user = await this.unifiedAuthResponse(
+            await this.getAUser({ id: loggedInUser.id })
+        )
+        return user
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} auth`
+    }
 }
